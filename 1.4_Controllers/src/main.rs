0c8535c09194421bf1ee6c2c2a4b8aa521c8d209
Charles Horn <charles.horn@gmail.com>
2025-05-10 10:44:21 +1200

    move textures to shared images/ dir

--- 1.4_Controllers/src/main.rs
+++ 1.4_Controllers/src/main.rs
@@ -52,9 +52,9 @@ fn main() {
         Framebuffer::new((0, 0), (0, 256), (320, 256), VideoMode::PAL, Some(INDIGO)).unwrap()
     };
     let texture_tim = if NTSC {
-        include_tim!("../../1.3_Textures/texture64_320x240-NTSC.tim")
+        include_tim!("../../images/texture64_320x240-NTSC.tim")
     } else {
-        include_tim!("../../1.3_Textures/texture64_320x256-PAL.tim")
+        include_tim!("../../images/texture64_320x256-PAL.tim")
     };
 
     let mut gpu_dma = dma::GPU::new();
